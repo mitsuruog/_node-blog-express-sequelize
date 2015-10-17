173465c95334770889b296afde5f77ade1919193
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-17 22:11:31 +0900

    Add REST APIのひな形を追加した

---
+++ api/article/article.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.findAll = function(req, res) {
+}
+
+exports.create = function(req, res) {
+}
+
+exports.update = function(req, res) {
+}
+
+exports.remove = function(req, res) {
+}
---
+++ api/article/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./article.controller');
+
+var router = express.Router();
+
+router.get('/', controller.findAll);
+router.post('/', controller.create);
+router.put('/id:', controller.update);
+router.delete('/id:', controller.remove);
+
+module.exports = router;
