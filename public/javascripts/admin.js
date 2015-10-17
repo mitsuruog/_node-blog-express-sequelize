a92a9df00781ab2a7d27f3e2a012dd31fbb99558
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-17 23:02:03 +0900

    Add 記事削除処理のフロント側の処理を追加した

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -5,7 +5,22 @@ console.log('I am admin :).');
 
   angular.module('app', []).controller('AdminController', AdminController);
 
-  function AdminController($scope, $http) {
+  function AdminController($scope, $http, $window) {
+
+    $scope.removeArticle = removeArticle;
+
+    ///////////
+
+    function removeArticle(id) {
+      $http({
+        method: 'DELETE',
+        url: '/api/articles/' + id
+      }).then(function(response){
+        // 削除が成功した場合は、画面をリフレッシュする
+        $window.location.reload();
+      });
+    }
+
   }
 
 })();
