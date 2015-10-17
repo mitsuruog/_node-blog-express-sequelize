da0f5b9512dbf966bd6e0e3ad4758fd8beb11055
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-17 22:19:52 +0900

    Add 初期表示時に公開中の記事を取得するようにした

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 var express = require('express');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    articles: [{
-      title: 'Mastering Node',
-      slug: 'mastering-node'
+  req.collections.articles.find({
+      published: true
     }, {
-      title: 'Practical Node.js',
-      slug: 'practical-nodejs'
-    }]
+    sort: {
+      _id: -1
+    }
+  }).toArray((err, articles) => {
+    if(err) return next(err);
+    res.render('index', {
+      articles: articles
+    });
+  });
+});
   });
 });
 
