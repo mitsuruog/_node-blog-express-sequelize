185b8c87941e5ddfc59a58032ece79ace277d0cb
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-17 22:49:40 +0900

    Add angular.jsを導入した

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,1 +1,11 @@
 console.log('I am admin :).');
+
+(function() {
+  'use strict';
+
+  angular.module('app', []).controller('AdminController', AdminController);
+
+  function AdminController($scope, $http) {
+  }
+
+})();
