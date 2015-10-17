ce1881290c410ab9de5142e0211a38c930b3e914
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-17 23:00:31 +0900

    Fix URLパタメータのエイリアスが間違っていたため、パラメータが取得できない件

--- api/article/index.js
+++ api/article/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 router.get('/', controller.findAll);
 router.post('/', controller.create);
-router.put('/id:', controller.update);
-router.delete('/id:', controller.remove);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.remove);
 
 module.exports = router;
