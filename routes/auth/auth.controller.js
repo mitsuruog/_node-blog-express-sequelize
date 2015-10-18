6c693b85ef82a5c977360163a7db8e3e4da0f9ab
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-18 13:27:04 +0900

    Add ログイン処理を追加した

--- routes/auth/auth.controller.js
+++ routes/auth/auth.controller.js
@@ -5,7 +5,26 @@ exports.login = function(req, res) {
 }
 
 exports.authenticate = function(req, res) {
-  res.redirect('/admin');
+  if(!req.body.email
+    || !req.body.password) {
+    return res.render('login', {
+      error: 'Please enter your email and password'
+    });
+  }
+  req.collections.users.findOne({
+    email: req.body.email,
+    password: req.body.password
+  }, (err, user) => {
+    if(err) return next(err);
+    if(!user) {
+      return res.render('login', {
+        error: 'Incorrect email & password combination.'
+      });
+    }
+    req.session.user = user;
+    req.session.admin = user.admin;
+    res.redirect('/admin');
+  })
 }
 
 exports.logout = function(req, res) {
