fa7ae175b6454dfae0c25560cf39117f60244e20
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-20 00:18:32 +0900

    Improve 可読性を良くするため、DB周りの設定をモジュールに切り出した

--- config/express.js
+++ config/express.js
@@ -8,16 +8,6 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var mongoskin = require('mongoskin');
-var dbUrl = process.env.MONGOLAB_URI || 'mongodb://192.168.99.100:27017/blog';
-var db = mongoskin.db(dbUrl, {
-  safe: true
-});
-var collections = {
-  users: db.collection('users'),
-  articles: db.collection('articles')
-};
-
 app.locals.title = 'blog-express';
 
 // view engine setup
@@ -37,25 +27,8 @@ app.use(session({
   saveUninitialized: true
 }));
 
-// TODO
-// 全リクエストのたびにmondodbの中身をチェックしているけど
-// ここでやるべきかは再考の余地あり
-// というか動作してない！？
-app.use(function(req, res, next) {
-  if(!collections.articles || !collections.users) {
-    return next(new Error('No collections :('));
-  }
-  req.collections = collections;
-  return next();
-});
-
-app.use(function(res, req, next) {
-  // サーバー側でSessionを見て管理者判定する
-  if(req.session && req.session.admin) {
-    app.locals.admin = true;
-  }
-  next();
-});
+// Persistent
+require('./db')(app);
 
 // Page & REST API Routes
 require('./routes')(app);
