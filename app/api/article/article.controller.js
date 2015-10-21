f884d9bcf5a181210078326521770a7de0ed2e40
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-21 23:35:08 +0900

    Change REST API呼び出し部分をmongooseに変更した

--- app/api/article/article.controller.js
+++ app/api/article/article.controller.js
@@ -1,37 +1,50 @@
 'use strict';
 
+var Article = require('../../models/article');
+
 exports.findAll = function(req, res, next) {
-  req.collections.articles.find({}, {
-    sort: {
+  Article.find({})
+    .sort({
       _id: -1
-    }
-  }).toArray((err, articles) => {
-    if(err) return next(err);
-    res.json({
-      articles: articles
+    })
+    .exec()
+    .then((articles) => {
+      res.json({
+        articles: articles
+      });
+    }, (err) => {
+      return next(err);
     });
-  });
 }
 
 exports.update = function(req, res, next) {
-  req.collections.articles.findById(req.params.id, (err, article) => {
-    if(err) return next(err);
-    if(!article) return res.sendStatus(404);
-    // 記事の公開フラグを変更する
-    article.published = !article.published;
-    req.collections.articles.updateById(req.params.id, {
-      $set: article
-    }, (err, count) => {
-      if(err) return next(err);
-      res.json({
-        article: article
+
+  Article.findById(req.params.id)
+    .exec()
+    .then((article) => {
+      if (!article) return res.sendStatus(404);
+      // 記事の公開フラグを変更する
+      article.published = !article.published;
+      return article;
+    })
+    .then((article) => {
+      article.save().then(() => {
+        res.json({
+          article: article
+        });
       });
+    }, (err) => {
+      return next(err);
     });
-  });
+
 }
 
-exports.remove = function(req, res) {
-  req.collections.articles.removeById(req.params.id, (err) => {
-    res.sendStatus(204);
-  });
+exports.remove = function(req, res, next) {
+  Article.findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      res.sendStatus(204);
+    }, (err) => {
+      return next(err);
+    });
 }
