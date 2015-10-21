77587cb13b881cb45bfcbcfaee95fa506a0dc460
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-21 23:08:00 +0900

    Change mongoskinをやめてmongooseを導入した

--- config/db.js
+++ config/db.js
@@ -1,28 +1,11 @@
 'use strict';
 
+var mongoose = require('mongoose');
+var dbUrl = process.env.MONGOLAB_URI || 'mongodb://192.168.99.100:27017/blog';
+
+mongoose.connect(dbUrl);
+
 module.exports = function(app) {
-
-  var mongoskin = require('mongoskin');
-  var dbUrl = process.env.MONGOLAB_URI || 'mongodb://192.168.99.100:27017/blog';
-  var db = mongoskin.db(dbUrl, {
-    safe: true
-  });
-  var collections = {
-    users: db.collection('users'),
-    articles: db.collection('articles')
-  };
-
-  // TODO
-  // 全リクエストのたびにmondodbの中身をチェックしているけど
-  // ここでやるべきかは再考の余地あり
-  // というか動作してない！？
-  app.use(function(req, res, next) {
-    if(!collections.articles || !collections.users) {
-      return next(new Error('No collections :('));
-    }
-    req.collections = collections;
-    return next();
-  });
 
   app.use(function(res, req, next) {
     // サーバー側でSessionを見て管理者判定する
