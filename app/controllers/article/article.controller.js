6d9cf17822c195763589812b550e33d050bbb5d9
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-21 23:35:55 +0900

    Change Jade画面のコントローラー部分をmongooseに変更した

--- app/controllers/article/article.controller.js
+++ app/controllers/article/article.controller.js
@@ -1,35 +1,35 @@
 'use strict';
 
+var Article = require('../../models/article');
 
 exports.create = function(req, res, next) {
-  if(!req.body.title ||
-      !req.body.slug ||
-      !req.body.text) {
-      res.render('post', {
-        error: 'Fill title, slug and text'
-      });
-  }
-  // TODO ES6s
-  var article = {
+
+  var newArticle = {
     title: req.body.title,
     slug: req.body.slug,
     text: req.body.text,
   };
-  req.collections.articles.insert(article, (err, newArticle) => {
-    if(err) return next(err);
+
+  Article.create(newArticle).then((article) => {
     // TODO errorで正常系のメッセージ投げるの違和感あり・・・
     res.render('post', {
       error: 'Artical was added. Publish it on Admin page.'
     });
+  }, (err) => {
+    return next(err);
   });
+
 }
 
 exports.show = function(req, res, next) {
-  req.collections.articles.findOne({
-    slug: req.params.slug
-  }, (err, article) => {
-    if(err) return next(err);
-    if(!article.published) return res.send(404);
-    res.render('article', article);
-  });
+  Article.findOne({
+      slug: req.params.slug
+    })
+    .exec()
+    .then((article) => {
+      if (!article.published) return res.send(404);
+      res.render('article', article);
+    }, (err) => {
+      return next(err);
+    });
 }
