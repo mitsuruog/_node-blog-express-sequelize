3e1f337f767d99394573b44c7e44e47cb92a697a
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-23 00:22:31 +0900

    Change 既存の認証処理へのルーティングで実行する処理をpassportに置き換え。それに伴い既存のロジックを削除した

--- app/controllers/auth/auth.controller.js
+++ app/controllers/auth/auth.controller.js
@@ -1,36 +1,7 @@
 'use strict';
 
-var User = require('../../models/user');
-
 exports.login = function(req, res) {
   res.render('login');
-}
-
-exports.authenticate = function(req, res, next) {
-
-  if (!req.body.email || !req.body.password) {
-    return res.render('login', {
-      error: 'Please enter your email and password'
-    });
-  }
-
-  User.findOne({
-      email: req.body.email,
-      password: req.body.password
-    })
-    .exec()
-    .then((user) => {
-      if (!user) {
-        return res.render('login', {
-          error: 'Incorrect email & password combination.'
-        });
-      }
-      req.session.user = user;
-      req.session.admin = user.admin;
-      res.redirect('/admin');
-    }, (err) => {
-      return next(err);
-    });
 }
 
 exports.logout = function(req, res) {
--- app/controllers/auth/index.js
+++ app/controllers/auth/index.js
@@ -2,11 +2,18 @@
 
 var express = require('express');
 var controller = require('./auth.controller');
+var passport = require('passport');
 
 var router = express.Router();
 
 router.get('/login', controller.login);
-router.post('/login', controller.authenticate); // TODO 名前が微妙
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/admin',
+  failureRedirect: '/login',
+  failureFlash: 'Invalid email or password.'
+}));
+
 router.get('/logout', controller.logout);
 
 module.exports = router;
