b90d5bb8ee13b9b3f3d3d8b8d969202821892029
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-10-24 22:21:08 +0900

    Add 環境設定ファイルを追加した

---
+++ config/environment/test.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://192.168.99.100:27017/blog'
+  },
+  // Redis connection options
+  redis: {
+    uri: 'redis://192.168.99.100:6379'
+  },
+
+  seedDB: true
+
+};
