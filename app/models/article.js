edbbc2754356a095eb200ae45074a9b678c4bccc
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-11-03 23:18:46 +0900

    Change Modelクラスをmongoldbからpostgresqlに変更した

--- app/models/article.js
+++ app/models/article.js
@@ -1,41 +1,34 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+module.exports = (sequelize, DataTypes) => {
 
-var ArticleSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    default: 'New Post'
-  },
-  text: String,
-  published: {
-    type: Boolean,
-    default: false
-  }
-});
-
-//////////// Virtuals
-
-ArticleSchema
-  .virtual('slug')
-  .set((value) => {
-    return value.toLowerCase().replace(' ', '-');
+  return sequelize.define('Article', {
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        max: {
+          args: [120],
+          msg: 'Title is too long(120 max)'
+        }
+      }
+    },
+    text: {
+      type: DataTypes.STRING(1024),
+    },
+    slug: {
+      type: DataTypes.STRING,
+      set(val) {
+        this.setDataValue('slug', val.toLowerCase().replace(' ', '-'));
+      }
+    },
+    published: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {
+    underscored: true,
+    freezeTableName: true
   });
 
-//////////// Validations
-
-ArticleSchema
-  .path('title')
-  .validate((value) => {
-    return value.length <= 120;
-  }, 'Title is too long(120 max)');
-
-//////////// Hooks
-
-//////////// Instance Methods
-
-//////////// Static Methods
-
-module.exports = mongoose.model('Article', ArticleSchema);
+};
--- app/models/user.js
+++ app/models/user.js
@@ -1,41 +1,37 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+module.exports = (sequelize, DataTypes) => {
 
-var UserSchema = new Schema({
-  email: {
-    type: String,
-    lowercase: true
-  },
-  password: String,
-  admin: {
-    type: Boolean,
-    default: false
-  }
-});
+  return sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      get() {
+        return this.getDataValue('email');
+      },
+      set(val) {
+        this.setDataValue('email', val.toLowerCase());
+      },
+      validate: {
+        isEmail: {
+          msg: 'Invalid email format.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true
+      }
+    },
+    admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {
+    underscored: true,
+    freezeTableName: true
+  });
 
-//////////// Virtuals
-
-//////////// Validations
-
-UserSchema
-  .path('email')
-  .validate((value) => {
-    return /\S+@\S+\.\S+/.test(value);
-  }, 'Invalid email format.');
-
-//////////// Hooks
-
-//////////// Instance Methods
-
-UserSchema.methods = {
-  authenticate(password) {
-    // TODO 本当はパスワードハッシュ化したほうがいいよ。。。
-    return this.password === password;
-  }
-};
-
-//////////// Static Methods
-
-module.exports = mongoose.model('User', UserSchema);
+}
