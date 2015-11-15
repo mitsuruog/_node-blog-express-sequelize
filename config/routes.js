145d9dc70dd6dd535c5938854f53085fbfa3d57b
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-11-15 23:09:32 +0900

    Fix ログインしているにも関わらず、メニューが管理者モードになっていない件

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,13 @@
 
 module.exports = function(app) {
 
+  // 管理者ログイン済みの場合に管理者用メニューを表示するためのmiddleware
+  // [MEMO]画面側のルーティングの前でやらないと動作しない
+  app.use((req, res, next) => {
+    app.locals.admin = req.user && req.user.admin;
+    next();
+  });
+
   // Pages
   app.use('/', require('../app/controllers/index'));
   app.use('/auth', require('../app/controllers/auth'));
