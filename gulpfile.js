7e0c15ba506a69446aba507901f902d5408c0a52
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-11-19 01:52:35 +0900

    Add eslintとjscsを導入した

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@ var gulp = require('gulp');
 var del = require('del');
 var espower = require('gulp-espower');
 var mocha = require("gulp-mocha");
+var jscs = require('gulp-jscs');
+var eslint = require('gulp-eslint');
+
+var srcDir = [
+  'app/**/*.js',
+  'config/**/*.js',
+  'public/javascripts/**/*.js',
+  'test/**/*.js',
+  'gulpfile.js',
+];
 
 gulp.task('clean:test', () => {
   return del([
@@ -13,6 +23,19 @@ gulp.task('power-assert', () => {
   gulp.src('./test/**/*.js')
     .pipe(espower())
     .pipe(gulp.dest('./.tmp'));
+});
+
+gulp.task('jscs', () => {
+  gulp.src(srcDir)
+    .pipe(jscs())
+    .pipe(jscs.reporter());
+});
+
+gulp.task('lint', () => {
+  gulp.src(srcDir)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('test', ['power-assert'], () => {
